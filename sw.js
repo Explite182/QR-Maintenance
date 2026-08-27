72e01e20a0fc596fa344b27ce95671881fc85137
Explite182 <explite_tech@shaw.ca>
2026-08-26 17:32:36 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const SITEWORKS_SW_VERSION = "20260826-pump-points-drawer-52";
+const SITEWORKS_SW_VERSION = "20260826-pump-pit-width-53";
 const STATIC_CACHE = `siteworks-static-${SITEWORKS_SW_VERSION}`;
 
 const STATIC_ASSETS = [
-  "/styles.css?v=20260826-pump-points-drawer-52",
+  "/styles.css?v=20260826-pump-pit-width-53",
   "/keybox.css?v=20260809-key-search-25",
   "/keybox.js?v=20260809-key-wizard-22",
   "/monitoring-engine.js?v=20260805-monitoring-engine",
-  "/js/panel-hmi-standard-model.js?v=20260826-pump-points-drawer-52",
-  "/js/panel-hmi-siteworks-adapter.js?v=20260826-pump-points-drawer-52",
-  "/js/panel-hmi-standard-renderer.js?v=20260826-pump-points-drawer-52",
-  "/app.js?v=20260826-pump-points-drawer-52",
-  "/manifest.webmanifest?v=20260826-pump-points-drawer-52",
-  "/assets/equipment/pump-centrifugal-blue.png?v=20260826-pump-points-drawer-52",
-  "/icons/siteworks-icon-192.png?v=20260826-pump-points-drawer-52",
-  "/icons/siteworks-icon-512.png?v=20260826-pump-points-drawer-52"
+  "/js/panel-hmi-standard-model.js?v=20260826-pump-pit-width-53",
+  "/js/panel-hmi-siteworks-adapter.js?v=20260826-pump-pit-width-53",
+  "/js/panel-hmi-standard-renderer.js?v=20260826-pump-pit-width-53",
+  "/app.js?v=20260826-pump-pit-width-53",
+  "/manifest.webmanifest?v=20260826-pump-pit-width-53",
+  "/assets/equipment/pump-centrifugal-blue.png?v=20260826-pump-pit-width-53",
+  "/icons/siteworks-icon-192.png?v=20260826-pump-pit-width-53",
+  "/icons/siteworks-icon-512.png?v=20260826-pump-pit-width-53"
 ];
 
 self.addEventListener("install", (event) => {
