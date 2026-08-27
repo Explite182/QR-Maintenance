6102e27fd3a7a16156b2c6f084f26b21fc1d78d6
Explite182 <explite_tech@shaw.ca>
2026-08-26 18:36:04 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const SITEWORKS_SW_VERSION = "20260826-pump-default-clean-57";
+const SITEWORKS_SW_VERSION = "20260826-hvac-rtu-start-58";
 const STATIC_CACHE = `siteworks-static-${SITEWORKS_SW_VERSION}`;
 
 const STATIC_ASSETS = [
-  "/styles.css?v=20260826-pump-default-clean-57",
+  "/styles.css?v=20260826-hvac-rtu-start-58",
   "/keybox.css?v=20260809-key-search-25",
   "/keybox.js?v=20260809-key-wizard-22",
   "/monitoring-engine.js?v=20260805-monitoring-engine",
-  "/js/panel-hmi-standard-model.js?v=20260826-pump-default-clean-57",
-  "/js/panel-hmi-siteworks-adapter.js?v=20260826-pump-default-clean-57",
-  "/js/panel-hmi-standard-renderer.js?v=20260826-pump-default-clean-57",
-  "/app.js?v=20260826-pump-default-clean-57",
-  "/manifest.webmanifest?v=20260826-pump-default-clean-57",
-  "/assets/equipment/pump-centrifugal-blue.png?v=20260826-pump-default-clean-57",
-  "/icons/siteworks-icon-192.png?v=20260826-pump-default-clean-57",
-  "/icons/siteworks-icon-512.png?v=20260826-pump-default-clean-57"
+  "/js/panel-hmi-standard-model.js?v=20260826-hvac-rtu-start-58",
+  "/js/panel-hmi-siteworks-adapter.js?v=20260826-hvac-rtu-start-58",
+  "/js/panel-hmi-standard-renderer.js?v=20260826-hvac-rtu-start-58",
+  "/app.js?v=20260826-hvac-rtu-start-58",
+  "/manifest.webmanifest?v=20260826-hvac-rtu-start-58",
+  "/assets/equipment/pump-centrifugal-blue.png?v=20260826-hvac-rtu-start-58",
+  "/icons/siteworks-icon-192.png?v=20260826-hvac-rtu-start-58",
+  "/icons/siteworks-icon-512.png?v=20260826-hvac-rtu-start-58"
 ];
 
 self.addEventListener("install", (event) => {
